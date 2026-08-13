60224a40db21708cb2726b0f358881e442580bb4
Origin-Core <vidox.iran@gmail.com>
2026-08-13 23:05:16 +0330

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,8 @@
-import { createClient } from 
-"https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } 
+from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 
-const SUPABASE_URL = 
+const SUPABASE_URL =
 "https://sfypfvyjoszrsigkwrsf.supabase.co";
 
 
@@ -10,7 +10,8 @@ const SUPABASE_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmeXBmdnlqb3N6cnNpZ2t3cnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MzkxNzIsImV4cCI6MjEwMjIxNTE3Mn0.1iuTJRhwb5vgX48yMjjJrQL4pVf1XaA6BdESdaTNKLU";
 
 
-export const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+export const supabase =
+createClient(
+SUPABASE_URL,
+SUPABASE_KEY
 );
